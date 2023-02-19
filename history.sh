9d53855e827bc1b666b95ae2e31012b3a7b54cf1
naouichia <naim.aouichia@ext.ingroupe.com>
2023-02-19 14:30:22 +0100

    Add sh hisotry for share cmd between computer dev (only for me in first stage of developement)

---
+++ history.sh
@@ -0,0 +1,81 @@
+ cd appCloud
+ ls 
+ ls -al 
+ npm i --save express$
+ npm i --save express
+ npm i 
+ mv test-copilot.js app.js
+ node app.js
+ code ./
+ npm i -g nodemon
+ sudo npm i -g nodemon
+ nodemon app.js
+ npm i 
+ nodemon app.js
+ npm i --save express-fileupload
+ npm i --save express
+ npm i --save fs
+ npm i --save child_process
+ npm i --save uuid
+ npm i --save ln-service
+ npm run dev
+ npm i --save ln-service
+ npm i --save bitcoinjs-lib
+ 1511  bbp
+ 1512  bbc
+ npm run dev
+ npm i --save tiny-secp256k1
+ npm run dev
+ 1516  cd ~/Desktop
+ 1517  cd appCloud
+ 1518  code ./
+ npm run dev
+ npm i --save unzipper
+ npm-rc-kk
+ npm i --save unzipper
+ npm-rc-public
+ npm i --save unzipper
+ npm run dev
+ npm run start
+ PORT=4600 npm run start
+ PORT=4600 node app.js
+ node app.js 4600
+ npm i
+ node app.js 4600
+ npm cache clean --force
+ sudo npm cache clean --force
+ node app.js 4600
+ lsof -i7000
+ lsof -i :7000
+ kill 620
+ lsof -i :7001
+ node app.js 4600
+ lsof -i :7001
+ node app.js 4600
+ 1542  init-bash
+ 1543  edit-
+ 1544  edit-bash
+ node app.js 4600
+ ngrok http  --hostname=foo.example.com --host-header="127.0.0.1:4600" 127.0.0.1:4600
+ ngrok http  --host-header="127.0.0.1:4600" 127.0.0.1:4600
+ node app.js 4600
+ ngrok http  --host-header="127.0.0.1:4600" 127.0.0.1:4600 >> ./test.http.ngrok
+ ngrok http  --host-header="127.0.0.1:4600" 127.0.0.1:4600 > ./test.http.ngrok
+ ngrok http  --host-header="127.0.0.1:4600" 127.0.0.1:4600 
+ npm i --save http-proxy
+ node app.js 4600
+ npm i --save axios
+ node app.js 4600
+ lsof -i :7001
+ kill 89337
+ node app.js 4600
+ ngrok http  --host-header="127.0.0.1:4600" 127.0.0.1:4600
+ ls -la
+ 1561  cd ..
+ 1562  cd 
+ 1563  cd PersoWorkspace
+ 1564  ls -la 
+ 1565  git clone https://github.com/naouaich/decentralized-cloud-computing.git
+ 1566  cd decentralized-cloud-computing
+ 1567  code ./
+ 1568  git push
